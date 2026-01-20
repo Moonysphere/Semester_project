7b1dbd1fc437160a059b0246ae1f0add9b290da1
Axel Barbellion <axelbarbellion@gmail.com>
2026-01-20 10:13:45 +0100

    Refactor GetCreateRulerController to use render method

--- Lorebase/app/src/Controllers/Ruler/GetCreateRulerController.php
+++ Lorebase/app/src/Controllers/Ruler/GetCreateRulerController.php
@@ -10,22 +10,6 @@ class GetCreateRulerController extends AbstractController
 {
     public function process(Request $request): Response
     {
-        $filePath = __DIR__ . '/../../../views/ruler/CreateRuler.html';
-
-        if (!file_exists($filePath)) {
-            return new Response(
-                json_encode(['error' => 'Form not found']),
-                404,
-                ['Content-Type' => 'application/json']
-            );
-        }
-
-        $content = file_get_contents($filePath);
-
-        return new Response(
-            $content,
-            200,
-            ['Content-Type' => 'text/html; charset=utf-8']
-        );
+        return $this->render('ruler','CreateRuler');
     }
 }
